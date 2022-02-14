f15b7fcfca33efb268e672169575daf71f0a74cc
gsweeh <gsweeh@gmail.com>
2022-02-14 13:50:39 +0530

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  invite: '', // Invite Code is part of the Referral link: https://app.treasure.cloud/auth/signup?code=[INVITE CODE]
+  invite: '', // Invite Code is part of the Referral link: https://app.treasure.cloud/auth/signup?code=YzhlZWFmMmUtOGQ2ZC0xMWVjLWI0MzUtZWQzMTUxNGJmZDAxOmYwYTE0NjdhLTMyYzEtMTFlYi1iMWI4LTViYTQzMmY1ZjBkMA==
   password: 'SvnKg7zFwkLc9mh9PAAWt',
   screenshotsDir: './screenshots',
   runs: 120,
